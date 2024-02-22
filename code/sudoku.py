a69d931092843b28033ff945889847a6b655c39d
unknown <warrenau@oregonstate.edu>
2024-02-22 11:07:12 -0800

    MW second file.

--- code/sudoku.py
+++ code/sudoku.py
@@ -1,168 +1,149 @@
-rows = 9
-cols = 9
 u = 0
 board = {}
 view_board = {}
 remove_board = {}
 temp_board = {}
-#user_input = 0
-trials = 0
-for x in range(cols):
-    for y in range (rows):  
-       u = u + 1         
-       board[(x+1,y+1)] = []
-       print("digit", u)
-       user_input = input()
-       if user_input == '1':    
-            board[(x+1,y+1)] = [1]
-       elif user_input == '2':
-            board[(x+1,y+1)] = [2]
-       elif user_input == '3':
-            board[(x+1,y+1)] = [3]
-       elif user_input == '4':
-            board[(x+1,y+1)] = [4]
-       elif user_input == '5':
-            board[(x+1,y+1)] = [5]
-       elif user_input == '6':
-            board[(x+1,y+1)] = [6]
-       elif user_input == '7':
-            board[(x+1,y+1)] = [7]
-       elif user_input == '8':
-            board[(x+1,y+1)] = [8]
-       elif user_input == '9':
-            board[(x+1,y+1)] = [9]
-       remove_board[(x+1,y+1)] = []
-       # below can be replaced with else board[(x+1,y+1)] = list(range(1,10))
-for x in range(cols):
-    for y in range (rows):
-       if len(board[(x+1,y+1)]) != 1:    
-            board[(x+1,y+1)] = list(range(1,10))
-for trials in range(5):
-    for x in range(cols):
-        for y in range (rows):
-            for j in range(cols):
-                if len(board[(j+1,y+1)]) == 1:
-                    v = board[(j+1,y+1)]
+nine_list = [x for x in range(1,10)] # addition of lists to help indexing
+thre_list = [x for x in range(1,4)]
+# I do not know how to have the input be a list/array, would love to see how it could be done. End goal is for program to enter the numbers itself though
+for x in nine_list:
+    for y in nine_list:  
+        u += 1         
+        board[(x,y)] = [] # indexing fixed. ur welcome.
+        remove_board[(x,y)] = []
+        print("digit", u)
+        user_input = input()
+        if user_input == "":
+           board[(x,y)] = nine_list
+        else:
+            board[(x,y)] = [int(user_input)]
+for trials in range(10): # havent setup a while loop yet for a few reasons but is a good idea
+    for x in nine_list: # we probably dont need to go through each cell 3 times but when i tried to do it all at once it wasn't working. breaking it into 3 separate checks got it working lol
+        for y in nine_list:
+            for j in nine_list:
+                if len(board[(j,y)]) == 1:
+                    v = board[(j,y)]
                     g = v[0]
-                    if v != board[(x+1,y+1)] and g not in remove_board[(x+1,y+1)]:
-                        remove_board[(x+1,y+1)].append(g)
-    for x in range(cols):
-        for y in range (rows):
-            for i in range(rows):            
-                if len(board[(x+1,i+1)])== 1:
-                    v = board[(x+1,i+1)]
+                    if v != board[(x,y)] and g not in remove_board[(x,y)]:
+                        remove_board[(x,y)].append(g)
+    for x in nine_list:
+        for y in nine_list:
+            for i in nine_list:            
+                if len(board[(x,i)])== 1:
+                    v = board[(x,i)]
                     g = v[0]
-                    if v != board[(x+1,y+1)] and g not in remove_board[(x+1,y+1)]:
-                        remove_board[(x+1,y+1)].append(g)
-    for x in range(cols):
-        for y in range (rows):
-            if x < 2.9 and y < 2.9:
-                for q in range(3):
-                    for p in range(3):
-                        if len(board[q+1,p+1]) == 1:
-                            v = board[(q+1,p+1)]
+                    if v != board[(x,y)] and g not in remove_board[(x,y)]:
+                        remove_board[(x,y)].append(g)
+    for x in nine_list:
+        for y in nine_list:
+            if x < 3.9 and y < 3.9:
+                for q in thre_list:
+                    for p in thre_list:
+                        if len(board[q,p]) == 1:
+                            v = board[(q,p)]
                             g = v[0]
-                            if v != board[x+1,y+1] and g not in remove_board[(x+1,y+1)]:
-                                remove_board[(x+1,y+1)].append(g)
-            elif x < 5.9 and y < 2.9:
-                for q in range(3):
-                    for p in range(3):
-                        if len(board[q+4,p+1]) == 1:
-                            v = board[(q+4,p+1)]
+                            if v != board[x,y] and g not in remove_board[(x,y)]:
+                                remove_board[(x,y)].append(g)
+            elif x < 6.9 and y < 3.9:
+                for q in thre_list:
+                    for p in thre_list:
+                        if len(board[q+3,p]) == 1:
+                            v = board[(q+3,p)]
                             g = v[0]
-                            if v != board[x+1,y+1] and g not in remove_board[(x+1,y+1)]:
-                                remove_board[(x+1,y+1)].append(g) 
-            elif x < 8.9 and y < 2.9:
-                for q in range(3):
-                    for p in range(3):
-                        if len(board[q+7,p+1]) == 1:
-                            v = board[(q+7,p+1)]
+                            if v != board[x,y] and g not in remove_board[(x,y)]:
+                                remove_board[(x,y)].append(g) 
+            elif x < 9.9 and y < 3.9:
+                for q in thre_list:
+                    for p in thre_list:
+                        if len(board[q+6,p]) == 1:
+                            v = board[(q+6,p)]
                             g = v[0]
-                            if v != board[x+1,y+1] and g not in remove_board[(x+1,y+1)]:
-                                remove_board[(x+1,y+1)].append(g)
-            elif x < 2.9 and y < 5.9:
-                for q in range(3):
-                    for p in range(3):
-                        if len(board[q+1,p+4]) == 1:
-                            v = board[(q+1,p+4)]
+                            if v != board[x,y] and g not in remove_board[(x,y)]:
+                                remove_board[(x,y)].append(g)
+            elif x < 3.9 and y < 6.9:
+                for q in thre_list:
+                    for p in thre_list:
+                        if len(board[q,p+3]) == 1:
+                            v = board[(q,p+3)]
                             g = v[0]
-                            if v != board[x+1,y+1] and g not in remove_board[(x+1,y+1)]:
-                                remove_board[(x+1,y+1)].append(g)
-            elif x < 5.9 and y < 5.9:
-                for q in range(3):
-                    for p in range(3):
-                        if len(board[q+4,p+4]) == 1:
-                            v = board[(q+4,p+4)]
+                            if v != board[x,y] and g not in remove_board[(x,y)]:
+                                remove_board[(x,y)].append(g)
+            elif x < 6.9 and y < 6.9:
+                for q in thre_list:
+                    for p in thre_list:
+                        if len(board[q+3,p+3]) == 1:
+                            v = board[(q+3,p+3)]
                             g = v[0]
-                            if v != board[x+1,y+1] and g not in remove_board[(x+1,y+1)]:
-                                remove_board[(x+1,y+1)].append(g) 
-            elif x < 8.9 and y < 5.9:
-                for q in range(3):
-                    for p in range(3):
-                        if len(board[q+7,p+4]) == 1:
-                            v = board[(q+7,p+4)]
+                            if v != board[x,y] and g not in remove_board[(x,y)]:
+                                remove_board[(x,y)].append(g) 
+            elif x < 9.9 and y < 6.9:
+                for q in thre_list:
+                    for p in thre_list:
+                        if len(board[q+6,p+3]) == 1:
+                            v = board[(q+6,p+3)]
                             g = v[0]
-                            if v != board[x+1,y+1] and g not in remove_board[(x+1,y+1)]:
-                                remove_board[(x+1,y+1)].append(g)
-            elif x < 2.9 and y < 8.9:
-                for q in range(3):
-                    for p in range(3):
-                        if len(board[q+1,p+7]) == 1:
-                            v = board[(q+1,p+7)]
+                            if v != board[x,y] and g not in remove_board[(x,y)]:
+                                remove_board[(x,y)].append(g)
+            elif x < 3.9 and y < 9.9:
+                for q in thre_list:
+                    for p in thre_list:
+                        if len(board[q,p+6]) == 1:
+                            v = board[(q,p+6)]
                             g = v[0]
-                            if v != board[x+1,y+1] and g not in remove_board[(x+1,y+1)]:
-                                remove_board[(x+1,y+1)].append(g)
-            elif x < 5.9 and y < 8.9:
-                for q in range(3):
-                    for p in range(3):
-                        if len(board[q+4,p+7]) == 1:
-                            v = board[(q+4,p+7)]
+                            if v != board[x,y] and g not in remove_board[(x,y)]:
+                                remove_board[(x,y)].append(g)
+            elif x < 6.9 and y < 9.9:
+                for q in thre_list:
+                    for p in thre_list:
+                        if len(board[q+3,p+6]) == 1:
+                            v = board[(q+3,p+6)]
                             g = v[0]
-                            if v != board[x+1,y+1] and g not in remove_board[(x+1,y+1)]:
-                                remove_board[(x+1,y+1)].append(g) 
-            elif x < 8.9 and y < 8.9:
-                for q in range(3):
-                    for p in range(3):
-                        if len(board[q+7,p+7]) == 1:
-                            v = board[(q+7,p+7)]
+                            if v != board[x,y] and g not in remove_board[(x,y)]:
+                                remove_board[(x,y)].append(g) 
+            elif x < 9.9 and y < 9.9:
+                for q in thre_list:
+                    for p in thre_list:
+                        if len(board[q+6,p+6]) == 1:
+                            v = board[(q+6,p+6)]
                             g = v[0]
-                            if v != board[x+1,y+1] and g not in remove_board[(x+1,y+1)]:
-                                remove_board[(x+1,y+1)].append(g)   
-    for x in range(cols):
-        for y in range (rows):            
-            temp_board[(x+1,y+1)] = [t for t in board[(x+1,y+1)] if t not in remove_board[(x+1,y+1)]]
+                            if v != board[x,y] and g not in remove_board[(x,y)]:
+                                remove_board[(x,y)].append(g)   
+    for x in nine_list:
+        for y in nine_list:            
+            temp_board[(x,y)] = [t for t in board[(x,y)] if t not in remove_board[(x,y)]]
     board = temp_board
-    for x in range(cols):
-        for y in range(rows):
-            if len(board[(x+1,y+1)]) != 1:
-                temp_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                for i in range(cols):
-                    for num in board[(x+1,i+1)]:
+    for x in nine_list:
+        for y in nine_list:
+            if len(board[(x,y)]) != 1:
+                temp_list = [x for x in nine_list]
+                for i in nine_list:
+                    for num in board[(x,i)]:
                         if num in temp_list and i != y:
                             temp_list.remove(num)
                     if len(temp_list) == 0:
                         break
                 if len(temp_list) == 1:
-                    board[(x+1,y+1)] = temp_list
-    for x in range(cols):
-        for y in range(rows):
-            if len(board[(x+1,y+1)]) != 1:
-                temp_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                for j in range(rows):
-                    for num in board[(j+1,y+1)]:
+                    board[(x,y)] = temp_list
+    for x in nine_list:
+        for y in nine_list:
+            if len(board[(x,y)]) != 1:
+                temp_list = [x for x in nine_list]
+                for j in nine_list:
+                    for num in board[(j,y)]:
                         if num in temp_list and j != x:
                             temp_list.remove(num)
                     if len(temp_list) == 0:
                         break
                 if len(temp_list) == 1:
-                    board[(x+1,y+1)] = temp_list
-for x in range(cols):
-    for y in range (rows):
-        if len(board[(x+1,y+1)]) == 1:
-            view_board[(x+1,y+1)] = board[(x+1,y+1)] 
+                    board[(x,y)] = temp_list
+for x in nine_list:
+    for y in nine_list:
+        if len(board[(x,y)]) == 1:
+            view_board[(x,y)] = board[(x,y)] 
         else:
-           view_board[(x+1,y+1)] = 'x'
-for x in range(cols):
-    for y in range(rows):
-        print(view_board[(x+1,y+1)], end= '  ')
+           view_board[(x,y)] = 'x'
+for x in nine_list:
+    for y in nine_list:
+        print(view_board[(x,y)], end= '  ')
     print()
+# i want the program to automatically enter the values into the online sudoku website.
