69b2b517b4340e299e3c23dbabc00aac06d30311
Austin Warren <warrenau@oregonstate.edu>
2024-02-23 14:54:06 -0800

    created and implemented a function for handling the square testing and removal. it uses math instead of a bunch of if statements, so it should be more readable and a little faster.

--- code/sudoku.py
+++ code/sudoku.py
@@ -1,3 +1,6 @@
+import math
+
+# variable definitions
 rows = 9
 cols = 9
 u = 0
@@ -23,6 +26,16 @@ def remove_check(column,row):
         if v != board[(x,y)] and g not in remove_board[(x,y)]:
             remove_board[(x,y)].append(g)
 
+# function for checking squares
+def check_squares(x,y):
+    # this function tests the sudoku squares for values that can be removed from the current cell
+    x_bounds = [3*math.floor(x/3),3*math.ceil(x/3)]
+    y_bounds = [3*math.floor(y/3),3*math.ceil(y/3)]
+    for q in range(x_bounds[0],x_bounds[1]):
+        for p in range(y_bounds[0],y_bounds[1]):
+            remove_check(q,p)
+
+
 #####
 # Main code
 #####            
@@ -31,7 +44,7 @@ def remove_check(column,row):
 for x in range(cols):
     for y in range(rows):  
        u = u + 1         
-       board[(x,y)] = []  # I dont like the indexing you are using. why have +1 on everything?
+       board[(x,y)] = []
        print("digit", u)
        user_input = input()
        if user_input == "":
@@ -41,7 +54,7 @@ for x in range(cols):
        remove_board[(x,y)] = []
        
 
-while max(board,key=len) > 1 and trials < 100:     # for this, there should be some stop condition. a while loop based on the length of all of the board entries perhaps
+while max(board,key=len) > 1 and trials < 100:
     trials += 1
     # test the cell to see if any values can be removed from possibility bc they are already in other cells in the column, row, or square
     for x in range(cols):   
@@ -55,42 +68,7 @@ while max(board,key=len) > 1 and trials < 100:     # for this, there should be s
                 remove_check(x,i)
 
             # test squares -- will try to make a function. in the meantime, this should increase the y values first so the code has to check fewer times
-            if x < 2.9 and y < 2.9:
-                for q in range(3):
-                    for p in range(3):
-                        remove_check(q,p)
-            elif x < 5.9 and y < 2.9:
-                for q in range(3,6):
-                    for p in range(3):
-                        remove_check(q,p) 
-            elif x < 8.9 and y < 2.9:
-                for q in range(6,9):
-                    for p in range(3):
-                        remove_check(q,p)
-            elif x < 2.9 and y < 5.9:
-                for q in range(3):
-                    for p in range(3,6):
-                        remove_check(q,p)
-            elif x < 5.9 and y < 5.9:
-                for q in range(3,6):
-                    for p in range(3,6):
-                        remove_check(q,p) 
-            elif x < 8.9 and y < 5.9:
-                for q in range(6,9):
-                    for p in range(3,6):
-                        remove_check(q,p)
-            elif x < 2.9 and y < 8.9:
-                for q in range(3):
-                    for p in range(6,9):
-                        remove_check(q,p)
-            elif x < 5.9 and y < 8.9:
-                for q in range(3,6):
-                    for p in range(6,9):
-                        remove_check(q,p)
-            elif x < 8.9 and y < 8.9:
-                for q in range(6,9):
-                    for p in range(6,9):
-                        remove_check(q,p)   
+            check_squares(x,y)  
 
             # remove any values from main board that have been added to the remove list    
             board[(x,y)] = [t for t in board[(x,y)] if t not in remove_board[(x,y)]]
